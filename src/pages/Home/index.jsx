e33ea41d230722deba1b06577eb59ba186d56494
SerAyaKz <131340184+SerAyaKz@users.noreply.github.com>
2023-12-08 16:30:36 +0600

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -285,7 +285,6 @@ const Home = () => {
           )}
         </div>
       </div>
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdOh2K5r_v7Xa_OxxmEKb1rBO8dNj-jO6Zp1Xy5sWi2xmKqtQ/viewform?embedded=true" width="640" height="2127" frameborder="0" marginheight="0" marginwidth="0">Загрузка…</iframe>
 
       <Footer />
     </div>
